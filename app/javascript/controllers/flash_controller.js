c43ac64d8bb40c1f64a0cdfe91acd781d3cefd8e
leandroptavares@gmail.com <leandroptavares@gmail.com>
2025-03-02 20:32:28 +0100

    remove console logs from flash controller

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,13 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flash"
 export default class extends Controller {
   static targets = ["message"]
-
-
+  
   connect() {
-  console.log("the flash")
 
   setTimeout(() => {
-    console.log("removing the element")
     this.messageTarget.classList.add("hidden");
   }, 4000);
 }
