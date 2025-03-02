2ca521934c7565610e93667f898aa42a140598b5
leandroptavares@gmail.com <leandroptavares@gmail.com>
2025-03-02 12:57:14 +0100

    change schedule_time to datetime in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_26_084541) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_02_115430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_26_084541) do
     t.string "delivery_address"
     t.decimal "weight"
     t.decimal "distance"
-    t.date "scheduled_time"
+    t.datetime "scheduled_time"
     t.decimal "cost"
     t.string "driver_name"
     t.datetime "created_at", null: false
