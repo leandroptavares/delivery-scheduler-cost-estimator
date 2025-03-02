5ffc1fedfbea878e9f33cf11caf16bd7ca2b5096
leandroptavares@gmail.com <leandroptavares@gmail.com>
2025-03-02 14:49:13 +0100

    add filter toogle to mobile

---
+++ app/javascript/controllers/filter_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="filter"
+export default class extends Controller {
+  static targets = ["filterFields", "filterToogle"]
+  static isFilterVisible = false
+
+  connect() {
+    console.log("filter controller is here")
+  }
+
+  toogleFilters(event) {
+    console.log("Toogling Filters...")
+
+    if (!this.constructor.isFilterVisible) {
+      this.filterFieldsTarget.classList.remove("hidden")
+      this.filterToogleTarget.textContent = "Hide filters"
+      this.constructor.isFilterVisible = !this.constructor.isFilterVisible
+    } else {
+      this.filterFieldsTarget.classList.add("hidden")
+      this.filterToogleTarget.textContent = "Show filters"
+      this.constructor.isFilterVisible = !this.constructor.isFilterVisible
+    }
+
+  }
+}
